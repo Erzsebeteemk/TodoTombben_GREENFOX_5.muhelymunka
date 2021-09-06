1d1532ce930ba909e5abb818ba310df10aea0942
Erzsebeteemk <88439357+Erzsebeteemk@users.noreply.github.com>
2021-09-06 16:43:10 +0200

    tömb-objektum indítása

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let bevitel = 0
+let szam  = 0
+
+
+$("ujSor").click(() => {
+
+        bevitel = $("#inputMezo").val()
+                                        // ? obj........ez az obj1 elnevezésű objektum. Hogy jelöljem ezt a mindenkori függvényben??????????..............
+                                        // ? Így írjam az obj-et a fg-be?
+        let obj = {
+                    szoveg: bevitel,
+                    sorszam: "szam",
+                    szin: ["black", "red", "blue", "green"],
+                    megjegyzes: (""),
+                    egyeb: (""),
+                    torles: ("true")                
+        }
+
+        tomb.push("object")
+}
+)
+
+
+
+$(".szingomb")
